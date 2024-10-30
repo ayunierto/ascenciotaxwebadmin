89b38dd1b8e5661c7c22a8ca6e7112f5cc569101
Alcides Turruellas <ayunierto@gmail.com>
2024-10-30 17:00:19 -0500

    added sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { Button } from './components/ui/button';
+import Layout from './dashboard/layout';
 
 function App() {
   return (
     <>
-      <div className='flex h-screen items-center justify-center'>
-        <Button>Hi, world</Button>
-      </div>
+      <Layout></Layout>
     </>
   );
 }
